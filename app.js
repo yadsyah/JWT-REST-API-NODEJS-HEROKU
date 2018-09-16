74b6f1da7b9ab15720133217ea8b9f23a285f0b9
Dian Setiyadi <diyansetiyadi@gmail.com>
2018-09-16 14:18:44 +0700

    error handling

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use((req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    // console.log(err.message)
+    console.log(err.message)
     if (err.status === 404) {
         console.log('Path URL NOT FOUND!')
         res.status(404).json({
