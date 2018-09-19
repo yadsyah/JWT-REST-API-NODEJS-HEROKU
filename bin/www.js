2b35425e889967ca656bd06fd5217a066aa26fd4
Dian Setiyadi <diyansetiyadi@gmail.com>
2018-09-19 12:11:48 +0700

    small fixing www

--- bin/www.js
+++ bin/www.js
@@ -4,7 +4,10 @@ const app = require('../app'); // The express app we just created
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
+console.log('TCL: ---------------');
+console.log('TCL: port', port);
+console.log('TCL: ---------------');
 
 const server = http.createServer(app);
-console.log('Server Berhasil Jalan Pada Port ',port);
+// console.log('Server Berhasil Jalan Pada Port ',port);
 server.listen(port);
