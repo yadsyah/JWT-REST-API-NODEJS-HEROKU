b86c415b501901e1c3f4c00ecdc5f51fb586d804
Dian Setiyadi <diyansetiyadi@gmail.com>
2018-11-06 13:54:03 +0700

    fixing error handling auth

--- server/constant/UtilAuth.js
+++ server/constant/UtilAuth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+
 function validateUser(req, res, next) {
     jwt.verify(req.headers['x-access-token'],
         req.app.get('secretKey'), (err, decoded) => {
@@ -12,29 +13,35 @@ function validateUser(req, res, next) {
             console.log(err);
             console.log('------------------------------------');
             if (err) {
-                if (err.message == 'TokenExpiredError') {
+                if (err.name === 'TokenExpiredError' && err.message === 'jwt expired') {
                     return res.status(401).send({
-                        code: err.status,
+                        code: 99,
                         error: true,
                         message: 'Token Expired!',
                     })
-                } else if (err.name === 'JsonWebTokenError') {
+                } else if (err.name === 'JsonWebTokenError' && err.message === 'jwt must be provided') {
                     res.status(401).send({
-                        code: err.status,
+                        code: 99,
                         error: true,
                         message: 'Harus melakukan Authorization terlebih dahulu!',
                     })
-                } else if (err.message == 'jwt malformed') {
+                } else if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
                     res.status(401).send({
-                        code: err.status,
+                        code: 99,
                         error: true,
                         message: 'Token Format ERROR!'
                     })
+                } else if (err.name === 'JsonWebTokenError' && err.message === 'invalid token') {
+                    res.status(401).send({
+                        code: 99,
+                        error: true,
+                        message: err.message
+                    })
                 } else {
                     res.json({
-                        code: err.status,
+                        code: 99,
                         error: true,
-                        message: err.message,
+                        message: err.name + ' - ' + err.message,
                     })
                 }
             } else {
@@ -47,5 +54,5 @@ function validateUser(req, res, next) {
 }
 
 module.exports = {
-    validateUser:validateUser
+    validateUser: validateUser
 }
